7f78ced2402e7cbbf6d967bae8cde3ac8d051654
RzgarHosseini <razgar@gmail.com>
2019-01-20 15:34:17 +0000

    Update ALLOWED.R

--- ALLOWED.R
+++ ALLOWED.R
@@ -1,7 +1,7 @@
 ALLOWED <- function(genotypes,DAG,x){
   # This function determines the feasibility of a set of genotypes according to a given DAG of restrictions.
   # genotypes: the set of genotypes to be analyzed.
-  # DAG: the DAG of restrictions
+  # DAG: matrix representing the DAG of restrictions. (nrow>=1 , ncol=2), the last row is always c(0,0)). See step 4 (data preprocessing in the README file)
   # x: the number of mutations considered.
   vec<-matrix(1,nrow=(2^x),ncol=1)
   D<-dim(DAG)[1]
